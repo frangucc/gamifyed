df48469f509c85eddf1f12c468fb6e55c7a9c15e
nonche <nonche@nonches-MacBook-Pro.local>
2015-07-27 12:17:34 -0500

    first instructions to rushi to develop the bash stuff

---
+++ gamifyed-cli/cli-definitions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#####################################################
+# Author: Rushi Patel
+# Date: 2015/07/22
+#####################################################
+
+ls
+
+#github should update the bash file. Everytime that commaned, Gamifyed is running, 
+#we should make a call to github and then tell the user hold on, updating my memory from the cloud... :)
+#gamifyed should always restart the client at the gamifyed root director or something
+
+
+#after updating the CLI Definitions from Github
+
+# here we ask for users name
+curl 'https://gamifyed-cli.firebaseio.com/01.json'
